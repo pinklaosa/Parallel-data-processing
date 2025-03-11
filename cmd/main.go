1c7dc3e70d6cc8e2b4edabd5f1d295ada8cccc45
pinklaosa <rps53009@hotmail.com>
2025-03-11 20:55:29 +0700

    Refactor: change structure for easy understanding

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "parallel-data-processing/infrastructure"
+import "parallel-data-processing/internal/infrastructure"
 
 func main() {
 	CSVInfrastructure := infrastructure.NewCSVReader("/assets/GTG-2_MASTER_DATA.csv")
